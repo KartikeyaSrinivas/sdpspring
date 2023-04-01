186f71ae7b0197f24acd5d995c69573ddaa4e3a2
KartikeyaSrinivas <chkartikeya022gmail.com>
2023-04-01 11:41:32 +0530

    first

--- src/main/java/com/health/demo/SdpApplication.java
+++ src/main/java/com/health/demo/SdpApplication.java
@@ -2,12 +2,28 @@ package com.health.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
-public class SdpApplication {
-
-	public static void main(String[] args) {
+public class SdpApplication extends SpringBootServletInitializer 
+{
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+  {
+    return application.sources(SpringBootServletInitializer.class);
+  }
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(SdpApplication.class, args);
 	}
+	@Bean
+	  public RestTemplate getRestTemplate()
+	  {
+	    return new RestTemplate();
+	  }
 
 }
